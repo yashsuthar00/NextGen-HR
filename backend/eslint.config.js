73bea20fd7d09e10f50eb2d22a7048a1f02e00f7
yashsutahr00 <yashsuthar0309@gmail.com>
2025-02-22 18:14:34 +0530

    database setup

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,5 +1,4 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
+export default [{ languageOptions: { globals: globals.browser } }];
