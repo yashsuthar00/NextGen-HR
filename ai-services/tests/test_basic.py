934df81719fe907bf75cd64641b80344948f1da0
yashsuthar00 <yashsuthar0309@gmail.com>
2025-03-04 02:51:00 +0530

    🎉 setup frontend and ai-services
    frontend with reactJs and ai-servicies with python flask

---
+++ ai-services/tests/test_basic.py
@@ -0,0 +1,22 @@
+# tests/test_basic.py
+import os
+import sys
+
+# Add the backend project root to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+from app import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['message'] == 'Hello, World!'
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>NextGen-HR frontend</h1>
+    </>
+  )
+}
+
+export default App
